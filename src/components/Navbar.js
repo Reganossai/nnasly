f869fd524295aa3ea9b386510e96f9d686a6ad45
Reagan <ossaireagano@gmail.com>
2024-09-02 20:36:16 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1 className="logo">NNaSLy farms.</h1>
+      <ul className="navbarSupportedContent">
+        <li>About us</li>
+        <li>About us</li>
+        <li>About us</li>
+        <li>About us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
